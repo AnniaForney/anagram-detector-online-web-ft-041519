541b164cc88e2628b49133fbd587c297532d223f
Annia Forney <anniaforney@gmail.com>
2019-04-28 22:06:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :diaper
+
+  def initialize(diaper)
+    @diaper = diaper
+  end
+
+def match(array)
+  matches = []
+  array.each do |diaper|
+    matches << diaper if diaper.split('').sort == self.diaper.split('').sort
+  end
+  matches
+end
+
+end
